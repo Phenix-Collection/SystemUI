4d9a0860b1f0019331e8c6fbf12493da7abe09ac
mengzhiya <392824146@qq.com>
2017-02-11 11:01:27 +0800

    init qucii_sysui

---
+++ SystemUI/sysui-push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+adb remount
+adb push ./bin/SystemUI.apk system/priv-app/SystemUI/
+#adb shell am force-stop com.android.systemui
+PROCESS=`adb shell ps |grep $com.android.systemui|grep -v grep|grep -v PPID |awk '{ print $2}'`
+# com.android.systemui
+# com.android.settings
+
+for i in $PROCESS
+do
+  echo " adb shell Kill the $1 process [ $i ]"
+  adb shell kill -9 $i
+done
